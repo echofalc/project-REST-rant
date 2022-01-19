12ec1ab0fd4e1721f086bce5b30d0fd2b916ea74
Erick Falconi <e.alexander.f013@gmail.com>
2022-01-18 19:33:32 -0500

    part 3 partial bonus: added jsx for new page, show page, and edit page but need to link to index

---
+++ views/places/newPage.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function newPage () {
+    return (
+        <Def>
+            <main>
+                <h1>New Page</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = newPage
