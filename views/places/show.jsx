94c29f8a3106e412ebfba98de774582ff291515b
Erick Falconi <e.alexander.f013@gmail.com>
2022-01-24 11:39:13 -0500

    Added Edit and Delete buttons with routing

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,32 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <div>
+                <img src={ data.place.pic } />
+            </div>
+            <div>
+                <h2>Rating</h2>
+                <a>Not Rated</a>
+            </div>
+            <div>
+                <h2>Description</h2>
+                <a>Located in { data.place.city}, { data.place.state } and serving { data.place.cuisines }</a>
+            </div>
+            <div>
+                <h2>Comments</h2>
+                <a>No comments yet!</a>
+            </div>
+            <div className="buttons">
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+            </div>
+
           </main>
         </Def>
     )
